34a18513d248062a83081795ddcc3ed8af87bc8f
Io Dodon <iondodon2@gmail.com>
2020-02-17 19:42:30 +0200

    trying to fetch data

--- Lab1/app/src/main/java/com/utm/lab1/activity/ListPostsActivity.kt
+++ Lab1/app/src/main/java/com/utm/lab1/activity/ListPostsActivity.kt
@@ -1,27 +1,50 @@
 package com.utm.lab1.activity
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.utm.lab1.R
 import com.utm.lab1.adapter.PostCardAdapter
 import com.utm.lab1.model.Post
+import java.net.HttpURLConnection
+import java.net.URL
 
 class ListPostsActivity : AppCompatActivity() {
     private lateinit var postsRecyclerView: RecyclerView
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var viewManager: RecyclerView.LayoutManager
 
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun getPosts(): ArrayList<Post> {
+        val posts: ArrayList<Post> = ArrayList()
+
+        val url = URL("https://jsonplaceholder.typicode.com/posts")
+
+        with(url.openConnection() as HttpURLConnection) {
+            requestMethod = "GET"
+
+            println("\nSent 'GET' request to URL : $url; Response Code : $responseCode")
+
+            inputStream.bufferedReader().use {
+                it.lines().forEach { line ->
+                    Log.d("MY", line)
+                }
+            }
+        }
+
+        return posts
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_posts)
 
-        val posts = ArrayList<Post>()
-        posts.add(Post("Title 1", "Body 1"))
-        posts.add(Post("Title 2", "Body 2"))
-        posts.add(Post("Title 3", "Body 3"))
-        posts.add(Post("Title 4", "Body 4"))
+        val posts: ArrayList<Post> = this.getPosts()
 
         viewManager = LinearLayoutManager(this)
         viewAdapter = PostCardAdapter(posts)
