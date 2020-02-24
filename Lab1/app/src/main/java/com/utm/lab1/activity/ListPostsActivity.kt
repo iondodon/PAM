7f4ab38134e660d4756ac866e1242b2f1ca99474
Io Dodon <iondodon2@gmail.com>
2020-02-24 22:01:14 +0200

    recycle view remove item

--- Lab1/app/src/main/java/com/utm/lab1/activity/ListPostsActivity.kt
+++ Lab1/app/src/main/java/com/utm/lab1/activity/ListPostsActivity.kt
@@ -1,7 +1,13 @@
 package com.utm.lab1.activity
 
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.github.kittinunf.fuel.httpGet
@@ -16,6 +22,9 @@ class ListPostsActivity : AppCompatActivity() {
     private lateinit var postsRecyclerView: RecyclerView
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var viewManager: RecyclerView.LayoutManager
+
+    private var swipeBackground: ColorDrawable = ColorDrawable(Color.parseColor("#FF0000"))
+    private lateinit var deleteIcon: Drawable
 
     private fun getPosts() {
 
@@ -33,7 +42,7 @@ class ListPostsActivity : AppCompatActivity() {
                         val sType = object : TypeToken<List<Post>>() { }.type
                         val otherList = gson.fromJson<List<Post>>(data, sType)
                         val posts: ArrayList<Post>  = otherList as ArrayList<Post>
-                        
+
                         showRecycleView(posts)
                     }
                 }
@@ -47,6 +56,66 @@ class ListPostsActivity : AppCompatActivity() {
         postsRecyclerView = findViewById(R.id.postsRecyclerView)
         postsRecyclerView.layoutManager = viewManager
         postsRecyclerView.adapter = viewAdapter
+
+        deleteIcon = ContextCompat.getDrawable(this, R.drawable.ic_delete_24px)!!
+
+        val itemTouchHelperCallback = object:
+            ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, position: Int) {
+                (viewAdapter as PostCardAdapter).removeItem(viewHolder)
+            }
+
+            override fun onChildDraw(c: Canvas, recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder,
+                dX: Float, dY: Float, actionState: Int, isCurrentlyActive: Boolean) {
+
+                val itemView = viewHolder.itemView
+
+                val iconMargin = (itemView.height - deleteIcon.intrinsicHeight) / 2
+
+                if(dX > 0) {
+                    swipeBackground.setBounds(itemView.left, itemView.top, dX.toInt(), itemView.bottom)
+                    deleteIcon.setBounds(
+                        itemView.left + iconMargin,
+                        itemView.top + iconMargin,
+                        itemView.left + iconMargin + deleteIcon.intrinsicWidth,
+                        itemView.bottom - iconMargin
+                    )
+                } else {
+                    swipeBackground.setBounds(itemView.right + dX.toInt(), itemView.top, itemView.right, itemView.bottom)
+                    deleteIcon.setBounds(
+                        itemView.right - iconMargin - deleteIcon.intrinsicWidth,
+                        itemView.top + iconMargin,
+                        itemView.right - iconMargin,
+                        itemView.bottom - iconMargin
+                    )
+                }
+
+                swipeBackground.draw(c)
+
+                c.save()
+                if(dX > 0){
+                    c.clipRect(itemView.left, itemView.top, dX.toInt(), itemView.bottom)
+                } else {
+                    c.clipRect(itemView.right + dX.toInt(), itemView.top, itemView.right, itemView.bottom)
+                }
+                c.restore()
+
+                deleteIcon.draw(c)
+
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+            }
+        }
+
+        val itemTouchHelper = ItemTouchHelper(itemTouchHelperCallback)
+        itemTouchHelper.attachToRecyclerView(postsRecyclerView)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
--- Lab1/app/src/main/java/com/utm/lab1/adapter/PostCardAdapter.kt
+++ Lab1/app/src/main/java/com/utm/lab1/adapter/PostCardAdapter.kt
@@ -5,15 +5,32 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.utm.lab1.R
 import com.utm.lab1.model.Post
 
 class PostCardAdapter(private val postList: ArrayList<Post>):
     RecyclerView.Adapter<PostCardAdapter.ViewHolder>() {
 
+    private var removedPosition = 0
+    private lateinit var removedItem: Post
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.post_layout, parent, false)
         return ViewHolder(view)
+    }
+
+    fun removeItem(viewHolder: RecyclerView.ViewHolder) {
+        removedPosition = viewHolder.adapterPosition
+        removedItem = postList[viewHolder.adapterPosition]
+
+        postList.removeAt(viewHolder.adapterPosition)
+        notifyItemRemoved(viewHolder.adapterPosition)
+
+        Snackbar.make(viewHolder.itemView, "$removedItem deleted.", Snackbar.LENGTH_LONG).setAction("UNDO") {
+            postList.add(removedPosition, removedItem)
+            notifyItemInserted(removedPosition)
+        }.show()
     }
 
     override fun getItemCount(): Int {
@@ -31,5 +48,4 @@ class PostCardAdapter(private val postList: ArrayList<Post>):
         val textViewPostBody = itemView.findViewById(R.id.card_post_body) as TextView
 
     }
-
 }
