3e13d521843795b90e9cb91bf2d17586776d8c54
Io Dodon <iondodon2@gmail.com>
2020-04-05 20:40:14 +0300

    starting tu use coroutines

--- Lab3/app/src/main/java/com/example/lab3/MainActivity.kt
+++ Lab3/app/src/main/java/com/example/lab3/MainActivity.kt
@@ -23,7 +23,9 @@ class MainActivity : AppCompatActivity() {
         val appBarConfiguration = AppBarConfiguration(setOf(
             R.id.navigation_home,
             R.id.navigation_list_all_posts,
-            R.id.navigation_list_photos
+            R.id.navigation_list_photos,
+            R.id.navigation_create_post,
+            R.id.navigation_update_post
         ))
 
         setupActionBarWithNavController(navController, appBarConfiguration)
--- Lab3/app/src/main/java/com/example/lab3/ui/listAllPosts/ListAllPostsFragment.kt
+++ Lab3/app/src/main/java/com/example/lab3/ui/listAllPosts/ListAllPostsFragment.kt
@@ -1,21 +1,122 @@
 package com.example.lab3.ui.listAllPosts
 
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.lab3.R
-import com.example.lab3.worker.LoadPosts
+import com.example.lab3.adapter.PostCardAdapter
+import com.example.lab3.model.Post
+import com.example.lab3.service.ApiService
+import kotlinx.coroutines.*
+import retrofit2.Call
 
 
 class ListAllPostsFragment : Fragment() {
+    private lateinit var postsRecyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+
+    private var swipeBackground: ColorDrawable = ColorDrawable(Color.parseColor("#FF0000"))
+    private lateinit var deleteIcon: Drawable
+
+    private lateinit var root: View
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val root = inflater.inflate(R.layout.fragment_list_all_posts, container, false)
+        this.root = inflater.inflate(R.layout.fragment_list_all_posts, container, false)
 
-        LoadPosts().execute(root)
+        CoroutineScope(Dispatchers.Main).launch {
+            val posts = loadPostsAsync().await()
+            showRecycleView(posts)
+        }
 
         return root
+    }
+
+     private fun loadPostsAsync(): Deferred<ArrayList<Post>> = CoroutineScope(Dispatchers.IO).async {
+        try {
+            val apiService = ApiService()
+            val postsCall: Call<List<Post>> = apiService.getService().posts()
+            val posts = postsCall.execute()
+            return@async posts.body() as ArrayList<Post>
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+            return@async ArrayList<Post>()
+        }
+    }
+
+    private fun showRecycleView(posts: ArrayList<Post>) {
+        viewAdapter = PostCardAdapter(posts)
+        postsRecyclerView = this.root.findViewById(R.id.postsRecyclerView)
+        postsRecyclerView.layoutManager = LinearLayoutManager(this.context)
+        postsRecyclerView.adapter = viewAdapter
+
+        deleteIcon = ContextCompat.getDrawable(this.requireContext(), R.drawable.ic_delete_24px)!!
+
+        val itemTouchHelperCallback = object:
+            ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, position: Int) {
+                (viewAdapter as PostCardAdapter).removeItem(viewHolder)
+            }
+
+            override fun onChildDraw(c: Canvas, recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder,
+                                     dX: Float, dY: Float, actionState: Int, isCurrentlyActive: Boolean) {
+
+                val itemView = viewHolder.itemView
+
+                val iconMargin = (itemView.height - deleteIcon.intrinsicHeight) / 2
+
+                if(dX > 0) {
+                    swipeBackground.setBounds(itemView.left, itemView.top, dX.toInt(), itemView.bottom)
+                    deleteIcon.setBounds(
+                        itemView.left + iconMargin,
+                        itemView.top + iconMargin,
+                        itemView.left + iconMargin + deleteIcon.intrinsicWidth,
+                        itemView.bottom - iconMargin
+                    )
+                } else {
+                    swipeBackground.setBounds(itemView.right + dX.toInt(), itemView.top, itemView.right, itemView.bottom)
+                    deleteIcon.setBounds(
+                        itemView.right - iconMargin - deleteIcon.intrinsicWidth,
+                        itemView.top + iconMargin,
+                        itemView.right - iconMargin,
+                        itemView.bottom - iconMargin
+                    )
+                }
+
+                swipeBackground.draw(c)
+
+                c.save()
+                if(dX > 0){
+                    c.clipRect(itemView.left, itemView.top, dX.toInt(), itemView.bottom)
+                } else {
+                    c.clipRect(itemView.right + dX.toInt(), itemView.top, itemView.right, itemView.bottom)
+                }
+                c.restore()
+
+                deleteIcon.draw(c)
+
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+            }
+        }
+
+        val itemTouchHelper = ItemTouchHelper(itemTouchHelperCallback)
+        itemTouchHelper.attachToRecyclerView(postsRecyclerView)
     }
 }
